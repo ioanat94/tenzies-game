1b6e6f9b7644404b5235fb9db9c7ed66106ceaa4
ioanat94 <ioanatiplea94@gmail.com>
2022-05-09 11:07:01 +0300

    Added die components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import Die from './components/Die'
+
 function App() {
   return (
-    <main className="App">
-      
+    <main className='App'>
+      <div className='App--container'>
+        <Die value={1} />
+        <Die value={2} />
+        <Die value={3} />
+        <Die value={4} />
+        <Die value={5} />
+        <Die value={6} />
+        <Die value={1} />
+        <Die value={2} />
+        <Die value={3} />
+        <Die value={4} />
+      </div>
     </main>
   );
 }
