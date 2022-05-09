0f068f8c0436d9ea7ef9096b97ea4c02b9c6d8d1
ioanat94 <ioanatiplea94@gmail.com>
2022-05-09 11:30:42 +0300

    Added random generation of dice

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
+import React from 'react'
 import Die from './components/Die'
 
 function App() {
+  function allNewDice() {
+    const newDice = []
+    for (let i = 0; i < 10; i++) {
+      newDice.push(Math.floor((Math.random() * 6) + 1))
+    }
+    return newDice
+  }
+
+  const [dice, setDice] = React.useState(allNewDice())
+
+  const diceElements = dice.map(die => <Die value={die} />)
+
   return (
     <main className='App'>
       <div className='App--container'>
-        <Die value={1} />
-        <Die value={2} />
-        <Die value={3} />
-        <Die value={4} />
-        <Die value={5} />
-        <Die value={6} />
-        <Die value={1} />
-        <Die value={2} />
-        <Die value={3} />
-        <Die value={4} />
+        {diceElements}
       </div>
     </main>
   );
